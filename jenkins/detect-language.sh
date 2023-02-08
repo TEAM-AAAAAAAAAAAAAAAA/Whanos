207159c8a1958a89d0bb039fc033d5265ca5cdfa
osvegn <thomas.prudhomme@epitech.eu>
2023-02-08 19:45:26 +0100

    Create detect-language script

---
+++ jenkins/detect-language.sh
@@ -0,0 +1,22 @@
+#/bin/bash
+
+# Create a table named "available_languages" with these columns: "C", "java", "javascript", "python", "befunge", "go"
+available_languages=("C" "java" "javascript" "python" "befunge" "go" NONE)
+languages_criteria=("Makefile" "pom.xml" "package.json" "requirements.txt" "main.bf" "main.go" NONE)
+script_to_run=("deploy_c.sh" "deploy_java.sh" "deploy_javascript.sh" "deploy_python.sh" "deploy_befunge.sh" "deploy_go.sh" NONE)
+index=0
+
+for language in "${languages_criteria[@]}"; do
+    if [[ $language == "NONE" ]]; then
+        echo "No project found"
+        exit 1
+    fi
+    if [[ -f $language ]]; then
+        echo "Found $language"
+        echo "Running ${script_to_run[$index]}"
+        bash ${script_to_run[$index]}
+        exit 0
+    fi
+    let "index++"
+    done
+done
---
+++ jenkins/job_dsl.groovy
@@ -0,0 +1,31 @@
+job('Link-project') {
+    parameters{
+        stringParam('GIT_URL', null, 'Git URL')
+        stringParam('DISPLAY_NAME', null, 'Display Name')
+    }
+    steps {
+        dsl('''
+job("$DISPLAY_NAME") {
+    scm {
+        git {
+            remote {
+                name('origin')
+                url("$GIT_URL")
+            }
+        }
+    }
+    triggers {
+        scm('* * * * *')
+    }
+    wrappers {
+        preBuildCleanup()
+    }
+    steps {
+        // Find the language of the project
+        // Check if the project has changed
+        // If the project has changed, deploy the project on a kubernetes cluster
+        shell('echo "Hello World"')
+    }
+}'''.stripIndent())
+    }
+}

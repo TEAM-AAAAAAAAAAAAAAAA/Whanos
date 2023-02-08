1e0123858f5d97cd0fc24659b420d0774b96df1d
osvegn <thomas.prudhomme@epitech.eu>
2023-02-08 20:35:03 +0100

    Detect multiple projects in detect-language.sh script

--- jenkins/detect-language.sh
+++ jenkins/detect-language.sh
@@ -4,18 +4,30 @@ available_languages=("C" "java" "javascript" "python" "befunge" "go" NONE)
 languages_criteria=("Makefile" "pom.xml" "package.json" "requirements.txt" "main.bf" "main.go" NONE)
 script_to_run=("deploy_c.sh" "deploy_java.sh" "deploy_javascript.sh" "deploy_python.sh" "deploy_befunge.sh" "deploy_go.sh" NONE)
 index=0
+language_found=0
+language_index=0
 
 for language in "${languages_criteria[@]}"; do
     if [[ $language == "NONE" ]]; then
-        echo "No project found"
-        exit 1
+        if [[ ${#language_found[@]} == 1 ]]; then
+            echo "No project found"
+            exit 1
+        else
+            break
+        fi
     fi
     if [[ -f $language ]]; then
-        echo "Found $language"
-        echo "Running ${script_to_run[$index]}"
-        bash ${script_to_run[$index]}
-        exit 0
+        language_found=(${language_found[@]} $language)
+        language_index=$index
     fi
     let "index++"
-    done
 done
+
+if [[ ${#language_found[@]} > 2 ]]; then
+    echo "Multiple projects found"
+    exit 1
+fi
+
+echo "Found ${available_languages[$language_index]}"
+echo "Running ${script_to_run[$language_index]}"
+bash ${script_to_run[$language_index]}

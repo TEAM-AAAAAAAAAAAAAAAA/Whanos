9b0d91122bf24ba7b2a857a6b86fccc916b019b3
osvegn <thomas.prudhomme@epitech.eu>
2023-02-16 17:00:30 +0100

    Create 'Build all base images' job

--- jenkins/build_base_image.groovy
+++ jenkins/build_base_image.groovy
@@ -1,49 +1,19 @@
 folder('/Whanos base images') {
     description('Base images for Whanos')
 }
-job('/Whanos base images/whanos-c') {
-    steps {
-        shell('docker build -t whanos-c ./whanos-c/')
+
+available_languages = ['c', 'java', 'javascript', 'python', 'befunge', 'go']
+
+for (language in available_languages) {
+    job("/Whanos base images/whanos-${language}") {
+        steps {
+            shell('docker build -t whanos-${language} ./whanos-${language}/')
+        }
     }
 }
 
-job('/Whanos base images/whanos-java') {
-    steps {
-        shell('docker build -t whanos-java ./whanos-java/')
-    }
-}
-
-job('/Whanos base images/whanos-javascript') {
-    steps {
-        shell('docker build -t whanos-javascript ./whanos-javascript/')
-    }
-}
-
-job('/Whanos base images/whanos-python') {
-    steps {
-        shell('docker build -t whanos-python ./whanos-python/')
-    }
-}
-
-job('/Whanos base images/whanos-befunge') {
-    steps {
-        shell('docker build -t whanos-befunge ./whanos-befunge/')
-    }
-}
-
-job('/Whanos base images/whanos-go') {
-    steps {
-        shell('docker build -t whanos-go ./whanos-go/')
-    }
-}
-
-job('/Whanos base images/Build all base images') {
-    steps {
-        build job: '/Whanos base images/whanos-c', wait: true
-        build job: '/Whanos base images/whanos-java', wait: true
-        build job: '/Whanos base images/whanos-javascript', wait: true
-        build job: '/Whanos base images/whanos-python', wait: true
-        build job: '/Whanos base images/whanos-befunge', wait: true
-        build job: '/Whanos base images/whanos-go', wait: true
-    }
+freeStyleJob('/Whanos base images/Build all base images') {
+    publishers {
+        downstream(available_languages.collect { language -> "Whanos base images/whanos-$language" })
+	}
 }

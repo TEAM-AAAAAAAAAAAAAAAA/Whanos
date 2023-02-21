ce03f9cb2440f8f55383f0dae3540c37879655d0
majent4 <99695392+majent4@users.noreply.github.com>
2023-02-21 16:08:33 +0100

    scripts: add sudo perms to jenkins

--- scripts/install-jenkins.sh
+++ scripts/install-jenkins.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-curl https://jenkins-ci.org/debian/jenkins-ci.org.key | apt-key add -
-sh -c "echo deb http://pkg.jenkins-ci.org/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list"
-apt-get update
-apt-get install jenkins -y
+curl -L https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo sh -c "echo deb http://pkg.jenkins-ci.org/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list"
+sudo apt update
+sudo apt install jenkins -y
 service jenkins restart

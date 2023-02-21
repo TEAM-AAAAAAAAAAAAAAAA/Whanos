3d97b7d8697da310b7752f90a75975a5852b4c18
majent4 <99695392+majent4@users.noreply.github.com>
2023-02-21 17:22:07 +0100

    scripts: install default-jre before jenkins

--- scripts/install-jenkins.sh
+++ scripts/install-jenkins.sh
@@ -2,8 +2,9 @@
 
 set -e
 
+sudo apt install default-jre -y
 curl -L https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
 sudo sh -c "echo deb http://pkg.jenkins-ci.org/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list"
 sudo apt update
 sudo apt install jenkins -y
-service jenkins restart
+sudo systemctl start jenkins

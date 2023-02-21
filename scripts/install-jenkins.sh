893a2a99897a2d13e4c82cffea208bcf9fc60ecf
majent4 <99695392+majent4@users.noreply.github.com>
2023-02-21 15:54:34 +0100

    scripts: replace wget with curl

--- scripts/install-jenkins.sh
+++ scripts/install-jenkins.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | apt-key add -
+curl https://jenkins-ci.org/debian/jenkins-ci.org.key | apt-key add -
 sh -c "echo deb http://pkg.jenkins-ci.org/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list"
 apt-get update
 apt-get install jenkins -y

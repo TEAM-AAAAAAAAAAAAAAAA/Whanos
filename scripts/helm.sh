5c2ad95380d8befdebecb93a2ee8f45f12fb62de
majent4 <99695392+majent4@users.noreply.github.com>
2023-02-23 16:17:35 +0100

    jenkins: add helm deploy scripts

--- scripts/helm.sh
+++ scripts/helm.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
-if [ -f whanos.yml ]; then
-    printf "whanos.yml found\n"
-fi
+set -e
+
+GCP_PROJECT_ID=$1
+REPO_LANGUAGE=$2
+
+[ -f Dockerfile ] && \
+sudo docker build . -t "$GCP_PROJECT_ID"/whanos-"$REPO_LANGUAGE" ||
+sudo docker build . -t "$GCP_PROJECT_ID"/whanos-"$REPO_LANGUAGE" -f images/"$REPO_LANGUAGE"/Dockerfile.standalone
+
+sudo docker push "$GCP_PROJECT_ID"/whanos-"$REPO_LANGUAGE"
+
+[ -f whanos.yml ] && \
+helm upgrade whanos-"$REPO_LANGUAGE" ./kubernetes \
+--set image.name=whanos-"$REPO_LANGUAGE" \
+--set image.image="$GCP_PROJECT_ID"/whanos-"$REPO_LANGUAGE" \
+-f whanos.yml

211de918a83bd7b1b748d1059c01c2553746d0f7
osvegn <thomas.prudhomme@epitech.eu>
2023-02-23 16:34:21 +0100

    Jenkins: Fix link_project.groovy configuration by adding jobs in /Projects folder, and accept private repositories using ssh key

--- jenkins/link_project.groovy
+++ jenkins/link_project.groovy
@@ -1,16 +1,25 @@
+folder('/Projects') {
+    description('Projects linked by the link-project job')
+}
+
 job('/link-project') {
     parameters{
         stringParam('GIT_URL', null, 'Git URL')
         stringParam('DISPLAY_NAME', null, 'Display Name')
+        credentialsParam('CREDENTIAL') {
+            type('com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey')
+            description('SSH key to clone repository')
+        }
     }
     steps {
         dsl('''
-job("/$DISPLAY_NAME") {
+job("/Projects/$DISPLAY_NAME") {
     scm {
         git {
             remote {
                 name('origin')
                 url("$GIT_URL")
+                credentials("$CREDENTIAL")
             }
         }
     }

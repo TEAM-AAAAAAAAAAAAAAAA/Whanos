7c8f17955f6d2ec52eb223cf25cf7568227b991b
Cavonstavant <constant.vigneron@protonmail.com>
2023-02-23 23:49:12 +0100

    jenkins: build and push base images

--- jenkins/build_base_image.groovy
+++ jenkins/build_base_image.groovy
@@ -7,7 +7,11 @@ available_languages = ['c', 'java', 'javascript', 'python', 'befunge', 'go']
 for (language in available_languages) {
     job("/Whanos base images/whanos-${language}") {
         steps {
-            shell("docker build /images/${language} -t whanos-${language} -f /images/${language}/Dockerfile.base")
+            shell("docker login -u _json_key --password-stdin https://eu.gcr.io < ${JENKINS_HOME}/whanos_gcp_creds.json")
+            shell("""
+                docker build ${JENKINS_HOME}/images/${language} -t eu.gcr.io/nomadic-mesh-376709/whanos-${language} -f ${JENKINS_HOME}/images/${language}/Dockerfile.base
+                docker push eu.gcr.io/nomadic-mesh-376709/whanos-${language}
+            """.stripIndent())
         }
     }
 }

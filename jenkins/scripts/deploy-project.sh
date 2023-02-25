8c84d4ef80b07b9e94cceee2dd399fc7d40b10f7
majent4 <99695392+majent4@users.noreply.github.com>
2023-02-25 19:06:22 +0100

    helm: prepend gcr.io to docker push url

--- jenkins/scripts/deploy-project.sh
+++ jenkins/scripts/deploy-project.sh
@@ -8,5 +8,5 @@ REPO_LANGUAGE=$2
 [ -f whanos.yml ] && \
 helm upgrade whanos-"$REPO_LANGUAGE" /var/lib/jenkins/kubernetes \
 --set image.name=whanos-"$REPO_LANGUAGE" \
---set image.image="$GCP_PROJECT_ID"/whanos-"$REPO_LANGUAGE" \
+--set image.image=eu.gcr.io/"$GCP_PROJECT_ID"/whanos-"$REPO_LANGUAGE" \
 -f whanos.yml --install

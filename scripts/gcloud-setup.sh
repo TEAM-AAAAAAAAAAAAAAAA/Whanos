306596b847ac243c01b8b18ac9ede3be70d486fe
majent4 <99695392+majent4@users.noreply.github.com>
2023-02-25 14:24:37 +0100

    jenkins: add docker setup script

---
+++ scripts/gcloud-setup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+PROJECT_USER=$1
+PROJECT_ID=$2
+PROJECT_REGION=$3
+GCP_CREDENTIALS=$4
+
+gcloud auth activate-service-account "$PROJECT_USER"@"$PROJECT_ID".iam.gserviceaccount.com --key-file="$GCP_CREDENTIALS" --project="$PROJECT_ID"
+gcloud auth configure-docker "$PROJECT_REGION"-docker.pkg.dev
+# gcloud config set disable_prompts true
+gcloud config set compute/region "$PROJECT_REGION"
+gcloud container clusters get-credentials "$PROJECT_ID"-gke
